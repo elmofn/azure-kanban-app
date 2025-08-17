6649a8c272251873bfef1086e38e8fe4a3bd344a
elmo <pdm.elmo@gmail.com>
2025-08-17 10:45:44 -0300

    remove a api debug

--- api/src/functions/debug.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context, req) {
-  const clientId = process.env.GOOGLE_CLIENT_ID;
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    headers: { 'Content-Type': 'text/plain' },
-    body: `GOOGLE_CLIENT_ID lido pelo ambiente: ${clientId || 'NAO ENCONTRADO'}`
-  };
-}
